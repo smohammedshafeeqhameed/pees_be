0ef513855d1b979e96678f2f4f27e2559767ae5c
smohammedshafeeqhameed <smohammedshafeeqhameed@gmail.com>
2023-11-13 01:43:36 +0530

    Added files fix for cores error

--- pees/pees/settings.py
+++ pees/pees/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,6 +60,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'pees.urls'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # or the address where your React app is hosted
+    "https://sleep.govindsr.me/",
+]
 
 TEMPLATES = [
     {
